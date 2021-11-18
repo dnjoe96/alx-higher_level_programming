abbebbd5dc84ade07988b4cfb49da36294bc78e5
dnjoe96 <donjoedbest@gmail.com>
2021-11-18 03:10:00 -0800

    Import modeule

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 iadd = __import__('add_0').add
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(1, 2, iadd(1, 2)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, iadd(a, b)))
