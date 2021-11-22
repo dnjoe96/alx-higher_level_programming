5d1206a76272a5e00b1344600b93053f940a7fd4
dnjoe96 <donjoedbest@gmail.com>
2021-11-22 02:13:27 -0800

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
-        return 
+        return
     for row in matrix:
         for i in row:
             if row.index(i) != len(row) - 1:
