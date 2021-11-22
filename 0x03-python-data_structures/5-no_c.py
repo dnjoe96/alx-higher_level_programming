666eda702576bd6b3a516eec19381fc5442b98bf
dnjoe96 <donjoedbest@gmail.com>
2021-11-22 01:09:35 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    if my_string is None:
+        return
+    new_str = ""
+    for i in my_string:
+        if i in ["C", "c"]:
+            continue
+        new_str = new_str + i
+    return new_str
