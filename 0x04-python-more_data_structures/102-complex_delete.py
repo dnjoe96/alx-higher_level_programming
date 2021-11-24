11ee43436c12ed1819fab8728c012c7913281b84
dnjoe96 <donjoedbest@gmail.com>
2021-11-24 22:39:20 +0100

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if a_dictionary is None:
+        return
+
+    for k, val in list(a_dictionary.items()):
+        if val == value:
+            a_dictionary.pop(k)
+    return a_dictionary
