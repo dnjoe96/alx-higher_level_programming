ec34d8298add0419d5cc878098d7df9594aab68f
dnjoe96 <donjoedbest@gmail.com>
2021-11-24 03:58:28 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+
+    for elem in my_list:
+        if elem == search:
+            new_list.append(replace)
+        else:
+            new_list.append(elem)
+    return new_list
