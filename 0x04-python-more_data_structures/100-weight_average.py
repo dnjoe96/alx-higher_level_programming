de9bbf316d9478e94783e3655a10e4f8bcdb7427
dnjoe96 <donjoedbest@gmail.com>
2021-11-24 22:03:00 +0100

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
     _sum = 0
     count = 0
     for elem in my_list:
-        prod = elem[1]
+        prod = elem[0] * elem[1]
         _sum += prod
-        count += 1
+        count += elem[1]
     return _sum / count
