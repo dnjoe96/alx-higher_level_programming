1cd574aee98e094bacd04bc5f7a574db47fb436f
dnjoe96 <donjoedbest@gmail.com>
2021-12-06 07:49:08 -0800

    Resolving conflict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    _sum = 0
+    count = 0
+    for elem in my_list:
+        prod = elem[0] * elem[1]
+        _sum += prod
+        count += 1
+    return _sum / count
