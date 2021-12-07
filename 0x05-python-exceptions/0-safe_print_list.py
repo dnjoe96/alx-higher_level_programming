d94f4092de1b14a02d9702406353f262d193e042
dnjoe96 <donjoedbest@gmail.com>
2021-12-07 08:40:22 +0100

    Print Integer

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for n in range(x):
             print(my_list[n], end="")
+            count += 1
         print("")
-        return n + 1
+        return count
     except Exception:
         print("")
-        return n
+        return count
