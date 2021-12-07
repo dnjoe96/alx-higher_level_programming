c7688f090e6db307de811c77b28598ea913d3ed9
dnjoe96 <donjoedbest@gmail.com>
2021-12-07 08:41:58 +0100

    Pycode style

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for n in range(x):
+        item = my_list[n]
+        try:
+            print("{:d}".format(item), end="")
+            count += 1
+        except Exception:
+            continue
+    print("")
+    return count
