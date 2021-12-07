331bf3a5cfaf17bac90bf6c1d4aab32afc6b91fe
dnjoe96 <donjoedbest@gmail.com>
2021-12-07 08:20:50 +0100

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for n in range(x):
+            print(my_list[n], end="")
+        print("")
+        return n + 1
+    except Exception:
+        print("")
+        return n
