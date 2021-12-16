5f59766fa43cb27d879c1ce9bf2141ef93cf242b
dnjoe96 <donjoedbest@gmail.com>
2021-12-16 06:58:30 +0100

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
-'''module for shapes'''
+'''module for Shape'''
 
 
 class Rectangle:
     '''class for rectangle'''
 
     def __init__(self, width=0, height=0):
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if height < 0:
+            raise ValueError('height must be >= 0')
         self.__width = width
         self.__height = height
 
