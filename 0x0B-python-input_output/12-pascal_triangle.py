e65bdea813bf09dec3fb32ee2bb117af6bd50aad
dnjoe96 <donjoedbest@gmail.com>
2021-12-23 13:52:35 +0100

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module for task 12"""
+
+
+def pascal_triangle(n):
+    mainlist = []
+
+    for i in range(1, n + 1):
+        sublist = []
+        if i == 1:
+            mainlist.append([1])
+        elif i == 2:
+            mainlist.append([1, 1])
+        else:
+            sublist.append(1)
+            for j in range(len(mainlist[-1]) - 1):
+                sublist.append(mainlist[-1][j] + mainlist[-1][j + 1])
+            sublist.append(1)
+            mainlist.append(sublist)
+        
+    return mainlist
+   
