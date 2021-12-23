8611e9e4c1b560bfc5e8b4940e5e42e52bd0f12a
dnjoe96 <donjoedbest@gmail.com>
2021-12-23 12:13:59 +0100

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -25,6 +25,7 @@ class Student:
         return new_dict
 
     def reload_from_json(self, json):
-        self.first_name = json['first_name']
-        self.last_name = json['last_name']
-        self.age = json['age']
+        if bool(json):
+            self.first_name = json['first_name']
+            self.last_name = json['last_name']
+            self.age = json['age']
