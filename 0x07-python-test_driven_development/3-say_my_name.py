0387fbfc6bf25d0caeb9a48a3525114452ab2582
dnjoe96 <donjoedbest@gmail.com>
2021-12-30 11:56:26 -0800

    4-print_square.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for say_my_name function"""
 
+
 def say_my_name(first_name, last_name=""):
     """ say_my_name function """
     if type(first_name) is not str:
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" module for print_square function"""
+
+
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for x in range(size):
+        for y in range(size):
+            print("#", end="")
+        print("")
