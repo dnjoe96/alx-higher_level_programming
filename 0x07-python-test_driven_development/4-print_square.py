25921a703716f47ee1499f1f7736f57b990c12a5
dnjoe96 <donjoedbest@gmail.com>
2021-12-30 12:22:28 -0800

    Indentation

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,6 +3,7 @@
 
 
 def print_square(size):
+    """ function to print square of size size"""
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" module for print_indentation function """
+
+
+def text_indentation(text):
+    """ texr_indentation function """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    y = "-"
+    for x in text:
+        if y not in [".", "?", ":"]:
+            print(x, end="")
+        y = x
+        if x in [".", "?", ":"]:
+            print("\n")
