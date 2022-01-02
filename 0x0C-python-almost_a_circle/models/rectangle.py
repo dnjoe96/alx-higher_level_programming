44c60a3ae021edcaf6ffe3b096f5544286df08c1
dnjoe96 <donjoedbest@gmail.com>
2022-01-02 03:20:21 -0800

    Saving state of rectangle module

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -52,6 +52,30 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError('height must be > 0')
         self.__height = value
+    
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be > 0')
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be > 0')
+        self.__y = value
 
     def area(self):
         return (self.__height * self.__width)
@@ -88,26 +112,13 @@ class Rectangle(Base):
         y = args[4] if len(args) > 4 else self.__y
         y = kwargs['y'] if 'y' in kwargs.keys() else y
 
-        if type(width) is not int:
-            raise TypeError('width must be an integer')
-        if width < 0:
-            raise ValueError('width must be > 0')
-        if type(height) is not int:
-            raise TypeError('height must be an integer')
-        if height < 0:
-            raise ValueError('height must be > 0')
-        if type(x) is not int:
-            raise TypeError('x must be an integer')
-        if type(y) is not int:
-            raise TypeError('y must be an integer')
-        if x < 0:
-            raise ValueError('x must be >= 0')
-        if y < 0:
-            raise ValueError('y must be >= 0')
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    def to_dictionary(self):
+        return self.__dict__
 
     def __str__(self):
         return f"[Rectangle ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
