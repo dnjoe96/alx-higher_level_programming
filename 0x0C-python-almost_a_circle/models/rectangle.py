ecbf7fb5df264d96fde1181c1ef9b03d7cd5bd31
dnjoe96 <donjoedbest@gmail.com>
2022-01-03 19:31:34 +0100

    Fixed bug in update method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -164,4 +164,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns the string representation of an instance"""
-        return f"[Rectangle ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, \
+                self.x, self.y, self.width, self.height)
