943840c4fe852a3d23d2e845e9591624a0cf1428
dnjoe96 <donjoedbest@gmail.com>
2022-02-03 06:37:46 -0800

    Create Users

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
