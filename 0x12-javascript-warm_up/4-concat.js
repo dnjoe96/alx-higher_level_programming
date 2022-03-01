dd2b16bfc8ccbf01a74ab02d7ef7d62b09f8dc0e
dnjoe96 <donjoedbest@gmail.com>
2022-03-01 11:51:44 +0100

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const args = process.argv;
+
+if (args.length <= 2) {
+  console.log('undefined is undefined');
+} else if (args.length === 3) {
+  console.log(args[2] + ' is undefined');
+} else {
+  console.log(args[2] + ' is ' + args[3]);
+}
