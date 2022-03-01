cfdc5cf581b21d8f314d3b6dd521f16b9ea88c38
dnjoe96 <donjoedbest@gmail.com>
2022-03-01 21:48:49 +0100

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const newList = [];
+
+  for (let i = 0; i < list.length; i++) {
+    newList[i] = list[list.length -1 - i];
+  }
+
+  return newList;
+};
