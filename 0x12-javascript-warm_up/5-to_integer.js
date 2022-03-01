66a9308cb817eb560348280ec66e45fe45b2648c
dnjoe96 <donjoedbest@gmail.com>
2022-03-01 12:01:51 +0100

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const args = process.argv;
+if (args[2]) {
+  if (isNaN(args[2])) {
+    console.log('Not a number');
+  } else {
+    console.log('My number: ' + args[2]);
+  }
+} else {
+  console.log('Not a number');
+}
