e8a381a01f22ab44978a18374c93eb4551fb8b8f
dnjoe96 <donjoedbest@gmail.com>
2022-03-01 21:22:59 +0100

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,36 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width || this.height) {
+      for (let i = 0; i < this.height; i++) {
+        let string = '';
+        for (let j = 0; j < this.width; j++) {
+          string += 'x';
+        }
+        console.log(string);
+      }
+    }
+  }
+
+  rotate () {
+    const newWidth = this.height;
+    const newHeight = this.width;
+    this.width = newWidth;
+    this.height = newHeight;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
