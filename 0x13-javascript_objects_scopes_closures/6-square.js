2dab5c78943d609414fc9111fdcbcc90df89e430
dnjoe96 <donjoedbest@gmail.com>
2022-03-01 21:39:41 +0100

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const SquareFirst = require('./5-square');
+
+class Square extends SquareFirst {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'x';
+    }
+
+    if (this.width || this.height) {
+      for (let i = 0; i < this.height; i++) {
+        let string = '';
+        for (let j = 0; j < this.width; j++) {
+          string += c;
+        }
+        console.log(string);
+      }
+    }
+  }
+}
+
+module.exports = Square;
