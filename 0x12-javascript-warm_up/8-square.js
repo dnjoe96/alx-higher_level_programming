7dfb8fd00527ef6daaf3d61f0ba3f10d3dd67173
dnjoe96 <donjoedbest@gmail.com>
2022-03-01 18:15:34 +0100

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const args = process.argv;
+let string = '';
+if (args.length > 2) {
+  if (isNaN(args[2])) {
+    console.log('Missing size');
+  } else {
+    for (let i = 0; i < parseInt(args[2]); i++) {
+      for (let j = 0; j < parseInt(args[2]); j++) {
+        string += 'x';
+      }
+      console.log(string);
+      string = '';
+    }
+  }
+} else {
+  console.log('Missing size');
+}
