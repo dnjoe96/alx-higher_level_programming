88d9127eeb98276192ef37747c33ef40358fc4b4
dnjoe96 <donjoedbest@gmail.com>
2022-03-02 18:46:57 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const args = process.argv;
+
+if (args.length <= 3) {
+  console.log(0);
+} else {
+  let largest = 0;
+  let secondLargest = 0;
+  for (let i = 2; i <= args.length; i++) {
+    if (parseInt(args[i]) > largest) {
+      secondLargest = largest;
+      largest = parseInt(args[i]);
+    }
+  }
+  console.log(secondLargest);
+}
