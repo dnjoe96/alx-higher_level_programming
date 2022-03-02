760fffbeeb5fb0b2b89032f3f4be4b653554462b
dnjoe96 <donjoedbest@gmail.com>
2022-03-02 18:36:33 +0100

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const args = process.argv;
+
+function factorial (x) {
+  if (x === 0 || x === 1 || isNaN(x)) {
+    return (1);
+  }
+  return x * factorial(x - 1);
+}
+
+const x = parseInt(args[2]);
+console.log(factorial(x));
