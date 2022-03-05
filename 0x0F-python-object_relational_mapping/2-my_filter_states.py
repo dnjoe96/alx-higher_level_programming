ae3e88c14c9d177165de1eb9acaf31128bf7a2c3
dnjoe96 <donjoedbest@gmail.com>
2022-03-05 07:45:27 +0100

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute(
             "SELECT * FROM states\
-            WHERE name='%s'\
-            ORDER BY id ASC;" % dbInfo[3])
+            WHERE name='{}'\
+            ORDER BY id ASC;".format(dbInfo[3]))
 
     rows = cur.fetchall()
     for row in rows:
