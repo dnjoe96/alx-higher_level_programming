7d09c1839174abe078fe4753f4fff8c5330b3ba7
dnjoe96 <donjoedbest@gmail.com>
2022-03-05 07:04:34 +0100

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""Script to select all states"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    dbInfo = sys.argv[1:]
+    """
+    db = MySQLdb.connect(host='localhost', user='root',\
+             passwd='clearance', db='hbtn_0e_0_usa')
+    """
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=dbInfo[0],
+            passwd=dbInfo[1],
+            db=dbInfo[2],
+            charset='utf8'
+            )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
