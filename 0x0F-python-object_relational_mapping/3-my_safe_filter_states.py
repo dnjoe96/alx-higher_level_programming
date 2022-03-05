4fa31aec52fd62e9c9da1621410c0d06d42d7a8a
dnjoe96 <donjoedbest@gmail.com>
2022-03-05 09:22:59 +0100

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""Script to select all states with a certain name to avoid SQL injection"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    dbInfo = sys.argv[1:]
+    # db = MySQLdb.connect(host='localhost', user='root',\
+    #         passwd='clearance', db='hbtn_0e_0_usa')
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=dbInfo[0],
+            passwd=dbInfo[1],
+            db=dbInfo[2],
+            charset='utf8'
+            )
+
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states\
+            WHERE name=%s\
+            ORDER BY id ASC;", (dbInfo[3], ))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
