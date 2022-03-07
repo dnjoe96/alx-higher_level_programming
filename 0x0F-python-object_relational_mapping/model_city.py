60f74477114215f739823bc6ba225d5b45aae144
dnjoe96 <donjoedbest@gmail.com>
2022-03-07 09:12:05 +0100

    model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 
 class City(Base):
-    """ Database table model"""
+    """ Database table model for city table"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
