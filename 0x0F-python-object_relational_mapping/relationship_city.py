93d8d642bb4bc0013ee4cb3729c9d8d6f9b156a1
dnjoe96 <donjoedbest@gmail.com>
2022-03-11 06:23:15 +0100

    Relationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Module to connect to mysql with SQLAlchemy"""
 
-from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 import sys
 
@@ -14,12 +14,3 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]
-        ), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Module to connect to mysql with SQLAlchemy"""
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 import sys
@@ -15,12 +15,3 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade="all, delete", backref="state")
-
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]
-        ), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
