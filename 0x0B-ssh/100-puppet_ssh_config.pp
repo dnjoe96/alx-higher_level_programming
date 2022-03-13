371c7379e72f531e4650017c0b9a0bf9a09a46ef
dnjoe96 <donjoedbest@gmail.com>
2022-03-13 22:52:18 +0100

    First commit

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+
+  
+#make changes to our configuration file
+file { '/etc/ssh/ssh_config':
+  ensure => present
+}->
+file_line { 'pass no':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication yes',
+}->
+file_line { 'IdentityFile':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  match  => '^IdentityFile',
+}
