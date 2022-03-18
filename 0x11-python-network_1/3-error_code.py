efbf8cad43365249034a21c053d5cdb59760fbe7
dnjoe96 <donjoedbest@gmail.com>
2022-03-18 21:57:52 +0100

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Get response from a given URL"""
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    import urllib.error
+
+    req = urllib.request.Request(sys.argv[1])
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            http = response.read()
+        print(http.decode("utf-8"))
+    except urllib.error.URLError as e:
+        print(f'Error code: {e}')
